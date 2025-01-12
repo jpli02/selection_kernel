e9d304990a80e3780e96ab4708d63ca7473e71ca
jpli02 <jplee0202@gmail.com>
2025-01-12 17:36:02 -0600

    rewrite test

--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,8 @@
 import torch
+import torch.nn as nn
 from selection_kernel import selection_attention
 import argparse
+import math
 import gc
 import pandas as pd
 
@@ -11,8 +13,7 @@ def gpu_cleanup():
     gc.collect()
     torch.cuda.empty_cache()
 
-
-def test_create_tensors(Z, H, N_CTX, HEAD_DIM, dtype=torch.float16):
+def create_tensors(Z, H, N_CTX, HEAD_DIM, dtype=torch.float16):
     """
     Create tensors for attention computation.
     """
@@ -22,66 +23,80 @@ def test_create_tensors(Z, H, N_CTX, HEAD_DIM, dtype=torch.float16):
     v = torch.rand((Z, H, N_CTX, HEAD_DIM), dtype=dtype, device="cuda")
     return q, k, v
 
-
-def test_reference_computation(Z, H, N_CTX, HEAD_DIM, causal, dtype=torch.float16):
+def _make_causal_mask(
+    bsz: int, tgt_len: int, past_key_values_length: int, dtype: torch.dtype, device: torch.device):
     """
-    Perform reference attention computation on the GPU.
+    Make causal mask used for bi-directional self-attention.
     """
-    q, k, v = test_create_tensors(Z, H, N_CTX, HEAD_DIM, dtype)
+    mask = torch.full((tgt_len, tgt_len), torch.finfo(dtype).min, device=device)
+    mask_cond = torch.arange(mask.size(-1), device=device)
+    mask.masked_fill_(mask_cond < (mask_cond + 1).view(mask.size(-1), 1), 0)
+    mask = mask.to(dtype)
 
-    sm_scale = 0.5
-    M = torch.tril(torch.ones((N_CTX, N_CTX), device="cuda", dtype=dtype))
-    p_gpu = torch.matmul(q, k.transpose(2, 3)) * sm_scale
+    if past_key_values_length > 0:
+        mask = torch.cat([torch.zeros(tgt_len, past_key_values_length, dtype=dtype, device=device), mask], dim=-1)
+    return mask[None, None, :, :].expand(bsz, 1, tgt_len, tgt_len + past_key_values_length)
 
-    if causal:
-        p_gpu = torch.where(M == 0, float("-inf"), p_gpu)
-
-    p_gpu = torch.softmax(p_gpu, dim=-1)
-    ref_c_gpu = torch.sum(p_gpu, dim=2)
-    ref_out_gpu = torch.matmul(p_gpu, v)
-    return ref_out_gpu, ref_c_gpu
-
-
-def test_triton_computation(Z, H, N_CTX, HEAD_DIM, causal, dtype=torch.float16):
+def ref_attention(Z, H, N_CTX, HEAD_DIM, causal, dtype=torch.float16):
+    q, k, v = create_tensors(Z, H, N_CTX, HEAD_DIM, dtype)
+    attn_weights = torch.matmul(q, k.transpose(2,3)) / math.sqrt(HEAD_DIM)
+    
+    if causal:           
+        attention_mask = _make_causal_mask(
+            bsz=Z,
+            tgt_len=N_CTX,
+            past_key_values_length=0,
+            dtype=q.dtype,
+            device=q.device,
+        )
+            
+        if attention_mask is not None:
+            attn_weights = attn_weights + attention_mask
+            attn_weights = torch.max(
+                attn_weights, torch.tensor(torch.finfo(attn_weights.dtype).min)
+            )
+    
+    attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float16).to(q.dtype)
+    attn_output = torch.matmul(attn_weights, v)
+    cumulative_attn_map = attn_weights.sum(2)
+    return attn_output, cumulative_attn_map
+                
+def triton_attention(Z, H, N_CTX, HEAD_DIM, causal, dtype=torch.float16):
     """
     Perform Triton-based attention computation on the GPU.
     """
-    q, k, v = test_create_tensors(Z, H, N_CTX, HEAD_DIM, dtype)
-    sm_scale = 0.5
+    q, k, v = create_tensors(Z, H, N_CTX, HEAD_DIM, dtype)
+    sm_scale = 1.0 / math.sqrt(HEAD_DIM)
     tri_out, tri_c, tri_m = selection_attention(q, k, v, causal, sm_scale)
     return tri_out, tri_c, tri_m
 
-
-def test_attention(Z, H, N_CTX, HEAD_DIM, causal, dtype=torch.float16):
+def test_attention(Z, H, N_CTX, HEAD_DIM, causal=False, dtype=torch.float16):
+    """
+    Test to compare correctness of triton cmul attention kernel
+    """
     gpu_cleanup()
-    ref_out_gpu1, ref_c_gpu1 = test_reference_computation(Z, H, N_CTX, HEAD_DIM, causal, dtype)
+    # pytorch results
+    ref_out_gpu1, ref_c_gpu1 = ref_attention(Z, H, N_CTX, HEAD_DIM, causal, dtype)
     # Convert reference tensors to match dtype of Triton results
     ref_c_gpu1 = ref_c_gpu1.half()
     ref_out_gpu1 = ref_out_gpu1.half()
+    # triton results
+    tri_out_gpu, tri_c_gpu, tri_m_gpu = triton_attention(Z, H, N_CTX, HEAD_DIM, causal, dtype)
     
-    ref_out_gpu2, ref_c_gpu2 = test_reference_computation(Z, H, N_CTX, HEAD_DIM, causal, dtype)
-    # Convert reference tensors to match dtype of Triton results
-    ref_c_gpu2 = ref_c_gpu2.half()
-    ref_out_gpu2 = ref_out_gpu2.half()
-    
-    tri_out_gpu, tri_c_gpu, tri_m_gpu = test_triton_computation(Z, H, N_CTX, HEAD_DIM, causal, dtype)
-
-    
-
-    # save results
-    # pd.DataFrame(ref_c_gpu.cpu().numpy().flatten()).to_csv("/u/ndani/selection_kernel/reference_scores.csv", index=False, header=False, float_format="%.5f") 
-    # pd.DataFrame(tri_c_gpu.cpu().numpy().flatten()).to_csv("/u/ndani/selection_kernel/ours_scores.csv", index=False, header=False, float_format="%.5f")
-
     # Compare results
     print(f"Attention max diff: {(tri_out_gpu.half() - ref_out_gpu1).abs().max().item()}")
-    assert torch.allclose(ref_out_gpu1, tri_out_gpu.half(), atol=1e-2, rtol=0), "Attention output mismatch"
+    assert torch.allclose(ref_out_gpu1, tri_out_gpu.half(), atol=0.07, rtol=0), "Attention output mismatch"
     print("Attention check passed")
     
     print(f"accum score max diff: {(tri_c_gpu.half() - ref_c_gpu1).abs().max().item()}")
     assert torch.allclose(ref_c_gpu1, tri_c_gpu.half(), atol=0.05, rtol=0), "Attention score acc mismatch"
     print("Attention score acc check passed")
     
+    # save results
+    # pd.DataFrame(ref_c_gpu.cpu().numpy().flatten()).to_csv("/u/ndani/selection_kernel/reference_scores.csv", index=False, header=False, float_format="%.5f") 
+    # pd.DataFrame(tri_c_gpu.cpu().numpy().flatten()).to_csv("/u/ndani/selection_kernel/ours_scores.csv", index=False, header=False, float_format="%.5f")
 
+   
 
 if __name__ == "__main__":
     # Parse command-line arguments
